2f415dd0c3bcd5cdcc1457d9cd04fce9fb27028d
Maarten Manders <maarten.manders@dualinventive.com>
2016-02-05 17:19:41 +0100

    Added missing List import

--- src/android/com/synconset/CordovaHTTP/CordovaHttp.java
+++ src/android/com/synconset/CordovaHTTP/CordovaHttp.java
@@ -14,6 +14,7 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.List;
 
 import java.net.MalformedURLException;
 import java.net.URL;
